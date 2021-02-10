c9f8dfbcc9f22c59f16edb25039e28afe15e38ee
windrunner9292 <jhleefred@naver.com>
2021-02-09 18:25:29 -0600

    adding more components and propTypes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({ fav }){
-  return <h1> I like {fav}</h1>;
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "Bulgogi",
+    image: "https://assets.bonappetit.com/photos/57acd741f1c801a1038bc801/master/w_1280%2Cc_limit/basic-bulgogi.jpg",
+    rating: 4
+  },
+  {
+    id: 3,
+    name: "Kimbap",
+    image: "https://i0.wp.com/www.habkorea.net/wp-content/uploads/2019/11/gimbap-1.jpg?w=800&ssl=1",
+    rating: 6
+  },
+  {
+    id: 4,
+    name: "Samgyetang",
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 8
+  }
+]
+
+// function renderFood(dish){
+//   console.log(dish);
+//   return <Food name={dish.name} image={dish.image}></Food>
+// }
+
+function Food({ name, image, rating }){
+  return <div>
+    <h2> I like {name}</h2>
+    <h4> {rating}/5.0</h4> 
+    <img src={image} alt={name}/>
+  </div> 
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 
 function App() {
   return  (
-  <div> 
-    <h1>Hello</h1>
-    <Food fav="kimchi" />
-    <Food fav="ramen" />
-    <Food fav="삼겹살" />
-    <Food fav="소고기" />
-  </div>
+  <div>
+    {foodILike.map(dish => (<Food  
+      key={dish.id} 
+      name={dish.name} 
+      image={dish.image} 
+      rating={dish.rating} />))}
+  </div>  
   );
 }
 
